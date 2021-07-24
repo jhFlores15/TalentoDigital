b43c8ed8d51ad9f98a61014df87ca142a7effd77
Johanna <jflores@riskamerica.com>
2021-07-23 21:02:32 -0400

    cadenas

--- Spring Boot/firstproject/src/main/java/com/johfloresm/firstproject/Random.java
+++ Spring Boot/firstproject/src/main/java/com/johfloresm/firstproject/Random.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 // 1. Annotation
 @RestController
-public class FirstprojectApplication {
+public class Random {
     public static void main(String[] args) {
         SpringApplication.run(FirstprojectApplication.class, args);
     }
 
     // 1. Anotación
-    @RequestMapping("/")
+    @RequestMapping("/random")
     // 3. Método que se asigna a la Solicitud anterior.
     public String hello() { // 3
-        return "Hello client! How are you doing?";
+        return "Sping Boot is great! So easy to just respong with strings";
     }
 }
