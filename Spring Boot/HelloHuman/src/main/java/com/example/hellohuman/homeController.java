4ad8809597885940df35ca31be9c802599ba41a4
Johanna <jflores@riskamerica.com>
2021-07-24 16:49:40 -0400

    hello human

---
+++ Spring Boot/HelloHuman/src/main/java/com/example/hellohuman/homeController.java
@@ -0,0 +1,24 @@
+package com.example.hellohuman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class homeController {
+
+    @RequestMapping("")
+    public String index(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String lastName
+    ) {
+        if (name == null && lastName == null) {
+            return "¡Hello Human!";
+        } else {
+            return "¡Hello " + (name != null ? name : "") + " " + (lastName != null ? lastName : "") + "!";
+        }
+    }
+
+
+}
